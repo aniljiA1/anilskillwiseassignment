57b2290e7273a1d39d13cdaf0536d7affa1f10d4
aniljiA1 <eeeanilkumar1995@gmail.com>
2025-11-23 16:29:16 +0530

    message

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const productsRouter = require('./routes/products');
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
-// Routes
-app.use('/api/products', productsRouter);
-
-// Test Route
-app.get('/', (req, res) => {
-  res.send('Backend is Running ✔');
+// Home route for testing
+app.get("/", (req, res) => {
+  res.send("Backend is running... ");
 });
 
-// Start Server
+// Server start
 const PORT = process.env.PORT || 4000;
-const HOST = "0.0.0.0";
-app.listen(PORT, () => console.log(`Local:   http://localhost:${PORT}`));
-  
-  //console.log(`Server running on port ${PORT}`));
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
